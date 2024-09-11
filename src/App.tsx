fbc69f091f52313bc0f37825f3a4514f0ca49590
mohit191 <notsotypicaldeveloper@gmail.com>
2024-09-12 00:00:02 +0530

    add cross logic using state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-export default function Square() {
+import { useState } from "react";
+
+
+function Square() {
+  const [value, setValue] = useState("");
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (<button className="square" onClick={handleClick}>{value}</button>);
+}
+export default function Board() {
   return (
     <>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+       <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+       </div>
+
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   )
 }
